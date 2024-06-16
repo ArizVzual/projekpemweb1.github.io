e7c687f2b112e9333d0da2f17fcdf6d1f8ede6d5
omogiri <akhmadrizkirahmatullah75@gmail.com>
2024-06-16 14:44:06 +0800

    Kode Program Quiz

---
+++ soal_quiz.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Apa planet terbesar di tata surya?",
+  answer: "Jupiter",
+  options: [
+    "Uranus",
+    "Jupiter",
+    "Saturnus",
+    "Neptunus"
+  ]
+},
+  {
+  numb: 2,
+  question: "Apa planet yang memiliki kehidupan di dalamnya?",
+  answer: "Bumi",
+  options: [
+    "Mars",
+    "Venus",
+    "Jupiter",
+    "Bumi"
+  ]
+},
+  {
+  numb: 3,
+  question: "Apa planet yang memiliki cincin?",
+  answer: "Saturnus",
+  options: [
+    "Saturnus",
+    "Bumi",
+    "Uranus",
+    "Neptunus"
+  ]
+},
+  {
+  numb: 4,
+  question: "Planet apa yang berada di orbit terakhir ?",
+  answer: "Neptunus",
+  options: [
+    "Bumi",
+    "Uranus",
+    "Saturnus",
+    "Neptunus"
+  ]
+},
+  {
+  numb: 5,
+  question: "Apa planet terpanas di tata surya?",
+  answer: "Venus",
+  options: [
+    "Venus",
+    "Merkurius",
+    "Mars",
+    "Bumi"
+  ]
+},
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
